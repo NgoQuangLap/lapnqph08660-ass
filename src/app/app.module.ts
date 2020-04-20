9415624947a99a95199aa421b00f7a811e0afabc
NgoQuangLap <ngoquanglap0612@gmail.com>
2020-04-20 21:26:36 +0700

    1 file modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,16 @@ import { HelloComponent } from './hello.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductManagerComponent } from './product-manager/product-manager.component';
 import { ProductService } from './product.service';
+import { ProductAddComponent } from './product-add/product-add.component';
+import { ProductEditComponent } from './product-edit/product-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
     ProductManagerComponent,
+    ProductAddComponent,
+    ProductEditComponent,
 
   ],
   imports: [
