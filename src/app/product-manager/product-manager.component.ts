e592f04e5baa1c6f1ccb5ea0ac0725604d66f0f4
NgoQuangLap <ngoquanglap0612@gmail.com>
2020-04-20 21:55:59 +0700

    3 files modified

--- src/app/product-manager/product-manager.component.ts
+++ src/app/product-manager/product-manager.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
+import { Product } from '../Product';
+
 
 @Component({
   selector: 'app-product-manager',
@@ -6,10 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-manager.component.css']
 })
 export class ProductManagerComponent implements OnInit {
+  products: Product[];
+  selected: Product;
 
-  constructor() { }
-
-  ngOnInit() {
+  cconstructor(
+    private productService: ProductService
+  ) { 
+    console.log('constructor')
   }
 
+  ngOnInit() {
+    this.getProducts();
+  }
+getProducts(){
+   this.productService.getProducts().subscribe(data => {
+     this.products = data;
+    });
+  }
+
+  removeItem(id){
+    this.productService.removeProduct(id).subscribe(response => {
+      this.products = this.products.filter(product => product.id != response.id);
+    })
+  }
 }
